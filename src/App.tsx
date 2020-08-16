b6de2bdd5ca62d5904d35cfd6f04637908f3d68e
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2020-08-16 21:17:47 +0200

    feat(excel): implemented excel renderer package

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import {OutTable, ExcelRenderer} from 'react-excel-renderer';
 import './App.css';
 
 function App() {
+  const [cols, setCols] = React.useState([]);
+  const [rows, setRows] = React.useState([]);
+  const fileHandler = (event) => {
+    let fileObj = event.target.files[0];
+
+    //just pass the fileObj as parameter
+    ExcelRenderer(fileObj, (err, resp) => {
+      if(err){
+        console.log(err);            
+      }
+      else{
+        setCols(resp.cols);
+        setRows(resp.rows);
+      }
+    });               
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input type="file" onChange={fileHandler} style={{"padding":"10px"}} />
+     <OutTable data={rows} columns={cols} tableClassName="ExcelTable2007" tableHeaderRowClass="heading" />
     </div>
   );
 }
